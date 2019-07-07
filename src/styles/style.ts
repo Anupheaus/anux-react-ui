84de1e82406645ee10f7e5744237e026dd85e0d4
Anupheaus <tony.hales@moodinternational.com>
2019-07-08 00:37:44 +0100

    Added cssRaw and ability to import fonts when creating a style

--- src/styles/style.ts
+++ src/styles/style.ts
@@ -1,4 +1,4 @@
-import { createTypeStyle } from 'typestyle';
+import { createTypeStyle, cssRaw, cssRule } from 'typestyle';
 import { is, IMap } from 'anux-common';
 import { NestedCSSSelectors, NestedCSSProperties } from 'typestyle/lib/types';
 import { registerStyleTag } from './styleTags';
@@ -13,21 +13,29 @@ interface IStyleObject extends Omit<NestedCSSProperties, '$nest'>, IMap {
 
 export interface StyleDelegate {
   (...objects: IStyleObject[]): string;
-  cssRule(name: string, style: IStyleObject): void;
+  cssRule: typeof cssRule;
+  cssRaw: typeof cssRaw;
 }
 
 interface ICreateStylesConfig {
   name: string;
   priority: number;
+  importedFonts?: string[];
 }
 
-export function createStyle(config: ICreateStylesConfig): StyleDelegate {
+export function createStyle({
+  name,
+  priority,
+  importedFonts = [],
+}: ICreateStylesConfig): StyleDelegate {
   const styleTag = document.createElement('style');
-  styleTag.setAttribute('data-styles-for', config.name);
-  registerStyleTag(styleTag, config.priority);
+  styleTag.setAttribute('data-styles-for', name);
+  registerStyleTag(styleTag, priority);
   const instance = createTypeStyle(styleTag);
   const result: StyleDelegate = (...objects: IStyleObject[]) => instance.style(...objects);
   result.cssRule = instance.cssRule;
+  result.cssRaw = instance.cssRaw;
+  if (importedFonts.length > 0) { importedFonts.forEach(url => instance.cssRaw(`@import url('${url}');`)); }
   return result;
 }
 
